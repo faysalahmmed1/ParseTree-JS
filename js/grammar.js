97ca2ea392d7205c71597dc78ae60243d523f37c
faysalahmmed1 <faysalahmmedcse@gmail.com>
2024-09-24 10:17:17 +0600

    grammar function

--- js/grammar.js
+++ js/grammar.js
@@ -1,409 +1,381 @@
 
-// var EPSILON = '\'\'';
+var EPSILON = '\'\'';
 
-// function Grammar(text) {
-// 	// <PUBLIC>
+function Grammar(text) {
 	
-// 	this.alphabet = [];
-// 	this.nonterminals = [];
-// 	this.terminals = [];
-// 	this.rules = [];
-// 	this.firsts = new Object();
-// 	this.follows = new Object();
 	
-// 	this.toString = function() {
-// 		return this.rules.join('\n');
-// 	};
+	this.alphabet = [];
+	this.nonterminals = [];
+	this.terminals = [];
+	this.rules = [];
+	this.firsts = new Object();
+	this.follows = new Object();
 	
-// 	this.getRulesForNonterminal = function(nonterminal) {
-// 		var result = [];
+	this.toString = function() {
+		return this.rules.join('\n');
+	};
+	
+	this.getRulesForNonterminal = function(nonterminal) {
+		var result = [];
 		
-// 		for (var i in this.rules) {
-// 			var rule = this.rules[i];
+		for (var i in this.rules) {
+			var rule = this.rules[i];
 			
-// 			if (nonterminal == rule.nonterminal) {
-// 				result.push(rule);
-// 			}
-// 		}
+			if (nonterminal == rule.nonterminal) {
+				result.push(rule);
+			}
+		}
 		
-// 		return result;
-// 	};
+		return result;
+	};
 	
-// 	/**
-// 	 * @param sequence
-// 	 * <br>Array of symbols
-// 	 * @result
-// 	 * <br>Array of terminal symbols
-// 	 * <br>New
-// 	 */
-// 	this.getSequenceFirsts = function(sequence) {
-// 		var result = [];
-// 		var epsilonInSymbolFirsts = true;
+
+	this.getSequenceFirsts = function(sequence) {
+		var result = [];
+		var epsilonInSymbolFirsts = true;
 		
-// 		for (var j in sequence) {
-// 			var symbol = sequence[j];
-// 			epsilonInSymbolFirsts = false;
+		for (var j in sequence) {
+			var symbol = sequence[j];
+			epsilonInSymbolFirsts = false;
 			
-// 			if (isElement(symbol, this.terminals)) {
-// 				addUnique(symbol, result);
+			if (isElement(symbol, this.terminals)) {
+				addUnique(symbol, result);
 				
-// 				break;
-// 			}
+				break;
+			}
 			
-// 			for (var k in this.firsts[symbol]) {
-// 				var first = this.firsts[symbol][k];
+			for (var k in this.firsts[symbol]) {
+				var first = this.firsts[symbol][k];
 				
-// 				epsilonInSymbolFirsts |= first == EPSILON;
+				epsilonInSymbolFirsts |= first == EPSILON;
 				
-// 				addUnique(first, result);
-// 			}
+				addUnique(first, result);
+			}
 			
-// 			epsilonInSymbolFirsts |= this.firsts[symbol] == undefined || this.firsts[symbol].length == 0;
+			epsilonInSymbolFirsts |= this.firsts[symbol] == undefined || this.firsts[symbol].length == 0;
 			
-// 			if (!epsilonInSymbolFirsts) {
-// 				break;
-// 			}
-// 		}
+			if (!epsilonInSymbolFirsts) {
+				break;
+			}
+		}
 		
-// 		if (epsilonInSymbolFirsts) {
-// 			addUnique(EPSILON, result);
-// 		}
+		if (epsilonInSymbolFirsts) {
+			addUnique(EPSILON, result);
+		}
 		
-// 		return result;
-// 	};
+		return result;
+	};
 	
-// 	// </PUBLIC>
+
 	
-// 	// <INITIALIZATION>
+	initializeRulesAndAlphabetAndNonterminals(this);
+	initializeAlphabetAndTerminals(this);
+	initializeFirsts(this);
+	initializeFollows(this);
 	
-// 	initializeRulesAndAlphabetAndNonterminals(this);
-// 	initializeAlphabetAndTerminals(this);
-// 	initializeFirsts(this);
-// 	initializeFollows(this);
+
 	
-// 	// </INITIALIZATION>
-	
-// 	// <PRIVATE>
-	
-// 	/**
-// 	 * @param grammar
-// 	 * <br>Input-output
-// 	 */
-// 	function initializeRulesAndAlphabetAndNonterminals(grammar) {
-// 		var lines = text.split('\n');
+
+	function initializeRulesAndAlphabetAndNonterminals(grammar) {
+		var lines = text.split('\n');
 		
-// 		for (var i in lines) {
-// 			var line = lines[i].trim();
+		for (var i in lines) {
+			var line = lines[i].trim();
 			
-// 			if (line != '') {
-// 				var rule = new Rule(grammar, line);
+			if (line != '') {
+				var rule = new Rule(grammar, line);
 				
-// 				grammar.rules.push(rule);
+				grammar.rules.push(rule);
 				
-// 				if (grammar.axiom == undefined) {
-// 					grammar.axiom = rule.nonterminal;
-// 				}
+				if (grammar.axiom == undefined) {
+					grammar.axiom = rule.nonterminal;
+				}
 				
-// 				addUnique(rule.nonterminal, grammar.alphabet);
-// 				addUnique(rule.nonterminal, grammar.nonterminals);
-// 			}
-// 		}
-// 	}
+				addUnique(rule.nonterminal, grammar.alphabet);
+				addUnique(rule.nonterminal, grammar.nonterminals);
+			}
+		}
+	}
 	
-// 	/**
-// 	 * @param grammar
-// 	 * <br>Input-output
-// 	 */
-// 	function initializeAlphabetAndTerminals(grammar) {
-// 		for (var i in grammar.rules) {
-// 			var rule = grammar.rules[i];
-			
-// 			for (var j in rule.development) {
-// 				var symbol = rule.development[j];
-				
-// 				if (symbol != EPSILON && !isElement(symbol, grammar.nonterminals)) {
-// 					addUnique(symbol, grammar.alphabet);
-// 					addUnique(symbol, grammar.terminals);
-// 				}
-// 			}
-// 		}
-// 	}
 	
-// 	/**
-// 	 * @param grammar
-// 	 * <br>Input-output
-// 	 */
-// 	function initializeFirsts(grammar) {
-// 		var notDone;
-		
-// 		do {
-// 			notDone = false;
+	function initializeAlphabetAndTerminals(grammar) {
+		for (var i in grammar.rules) {
+			var rule = grammar.rules[i];
 			
-// 			for (var i in grammar.rules) {
-// 				var rule = grammar.rules[i];
-// 				var nonterminalFirsts = getOrCreateArray(grammar.firsts, rule.nonterminal);
+			for (var j in rule.development) {
+				var symbol = rule.development[j];
 				
-// 				if (rule.development.length == 1 && rule.development[0] == EPSILON) {
-// 					notDone |= addUnique(EPSILON, nonterminalFirsts);
-// 				} else {
-// 					notDone |= collectDevelopmentFirsts(grammar, rule.development, nonterminalFirsts);
-// 				}
-// 			}
-// 		} while (notDone);
-// 	}
+				if (symbol != EPSILON && !isElement(symbol, grammar.nonterminals)) {
+					addUnique(symbol, grammar.alphabet);
+					addUnique(symbol, grammar.terminals);
+				}
+			}
+		}
+	}
 	
-// 	/**
-// 	 * @param grammar
-// 	 * <br>Input-output
-// 	 * @param development
-// 	 * <br>Array of symbols
-// 	 * @param nonterminalFirsts
-// 	 * <br>Array of symbols
-// 	 * <br>Input-output
-// 	 * @return <code>true</code> If <code>nonterminalFirsts</code> has been modified
-// 	 */
-// 	function collectDevelopmentFirsts(grammar, development, nonterminalFirsts) {
-// 		var result = false;
-// 		var epsilonInSymbolFirsts = true;
-		
-// 		for (var j in development) {
-// 			var symbol = development[j];
-// 			epsilonInSymbolFirsts = false;
 	
-// 			if (isElement(symbol, grammar.terminals)) {
-// 				result |= addUnique(symbol, nonterminalFirsts);
-				
-// 				break;
-// 			}
-			
-// 			for (var k in grammar.firsts[symbol]) {
-// 				var first = grammar.firsts[symbol][k];
-				
-// 				epsilonInSymbolFirsts |= first == EPSILON;
-				
-// 				result |= addUnique(first, nonterminalFirsts);
-// 			}
-			
-// 			if (!epsilonInSymbolFirsts) {
-// 				break;
-// 			}
-// 		}
+	function initializeFirsts(grammar) {
+		var notDone;
 		
-// 		if (epsilonInSymbolFirsts) {
-// 			result |= addUnique(EPSILON, nonterminalFirsts);
-// 		}
-		
-// 		return result;
-// 	}
+		do {
+			notDone = false;
+			
+			for (var i in grammar.rules) {
+				var rule = grammar.rules[i];
+				var nonterminalFirsts = getOrCreateArray(grammar.firsts, rule.nonterminal);
+				
+				if (rule.development.length == 1 && rule.development[0] == EPSILON) {
+					notDone |= addUnique(EPSILON, nonterminalFirsts);
+				} else {
+					notDone |= collectDevelopmentFirsts(grammar, rule.development, nonterminalFirsts);
+				}
+			}
+		} while (notDone);
+	}
 	
-// 	/**
-// 	 * @param grammar
-// 	 * <br>Input-output
-// 	 */
-// 	function initializeFollows(grammar) {
-// 		var notDone;
+	
+	function collectDevelopmentFirsts(grammar, development, nonterminalFirsts) {
+		var result = false;
+		var epsilonInSymbolFirsts = true;
 		
-// 		do {
-// 			notDone = false;
-			
-// 			for (var i in grammar.rules) {
-// 				var rule = grammar.rules[i];
+		for (var j in development) {
+			var symbol = development[j];
+			epsilonInSymbolFirsts = false;
+	
+			if (isElement(symbol, grammar.terminals)) {
+				result |= addUnique(symbol, nonterminalFirsts);
 				
-// 				if (i == 0) {
-// 					var nonterminalFollows = getOrCreateArray(grammar.follows, rule.nonterminal);
+				break;
+			}
+			
+			for (var k in grammar.firsts[symbol]) {
+				var first = grammar.firsts[symbol][k];
+				
+				epsilonInSymbolFirsts |= first == EPSILON;
+				
+				result |= addUnique(first, nonterminalFirsts);
+			}
+			
+			if (!epsilonInSymbolFirsts) {
+				break;
+			}
+		}
+		
+		if (epsilonInSymbolFirsts) {
+			result |= addUnique(EPSILON, nonterminalFirsts);
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * @param grammar
+	 * <br>Input-output
+	 */
+	function initializeFollows(grammar) {
+		var notDone;
+		
+		do {
+			notDone = false;
+			
+			for (var i in grammar.rules) {
+				var rule = grammar.rules[i];
+				
+				if (i == 0) {
+					var nonterminalFollows = getOrCreateArray(grammar.follows, rule.nonterminal);
 					
-// 					notDone |= addUnique('$', nonterminalFollows);
-// 				}
+					notDone |= addUnique('$', nonterminalFollows);
+				}
 				
-// 				for (var j in rule.development) {
-// 					var symbol = rule.development[j];
+				for (var j in rule.development) {
+					var symbol = rule.development[j];
 					
-// 					if (isElement(symbol, grammar.nonterminals)) {
-// 						var symbolFollows = getOrCreateArray(grammar.follows, symbol);
-// 						var afterSymbolFirsts = grammar.getSequenceFirsts(rule.development.slice(parseInt(j) + 1));
+					if (isElement(symbol, grammar.nonterminals)) {
+						var symbolFollows = getOrCreateArray(grammar.follows, symbol);
+						var afterSymbolFirsts = grammar.getSequenceFirsts(rule.development.slice(parseInt(j) + 1));
 						
-// 						for (var k in afterSymbolFirsts) {
-// 							var first = afterSymbolFirsts[k];
+						for (var k in afterSymbolFirsts) {
+							var first = afterSymbolFirsts[k];
 							
-// 							if (first == EPSILON) {
-// 								var nonterminalFollows = grammar.follows[rule.nonterminal];
+							if (first == EPSILON) {
+								var nonterminalFollows = grammar.follows[rule.nonterminal];
 								
-// 								for (var l in nonterminalFollows) {
-// 									notDone |= addUnique(nonterminalFollows[l], symbolFollows);
-// 								}
-// 							} else {
-// 								notDone |= addUnique(first, symbolFollows);
-// 							}
-// 						}
-// 					}
-// 				}
-// 			}
-// 		} while (notDone);
-// 	}
+								for (var l in nonterminalFollows) {
+									notDone |= addUnique(nonterminalFollows[l], symbolFollows);
+								}
+							} else {
+								notDone |= addUnique(first, symbolFollows);
+							}
+						}
+					}
+				}
+			}
+		} while (notDone);
+	}
 	
-// 	// </PRIVATE>
-// }
+	// </PRIVATE>
+}
 
-// function Rule(grammar, text) {
-// 	// <PUBLIC>
+function Rule(grammar, text) {
+	// <PUBLIC>
 	
-// 	this.grammar = grammar;
-// 	this.index = grammar.rules.length;
+	this.grammar = grammar;
+	this.index = grammar.rules.length;
 	
-// 	var split = text.split('->');
+	var split = text.split('->');
 	
-// 	this.nonterminal = split[0].trim();
+	this.nonterminal = split[0].trim();
 	
-// 	this.development = trimElements(split[1].trim().split(' '));
+	this.development = trimElements(split[1].trim().split(' '));
 	
-// 	this.toString = function() {
-// 		return this.nonterminal + ' -> ' + this.development.join(' ');
-// 	};
+	this.toString = function() {
+		return this.nonterminal + ' -> ' + this.development.join(' ');
+	};
 
-// 	this.equals = function(that) {
-// 		if (this.nonterminal != that.nonterminal) {
-// 			return false;
-// 		}
+	this.equals = function(that) {
+		if (this.nonterminal != that.nonterminal) {
+			return false;
+		}
 		
-// 		if (parseInt(this.development.length) != parseInt(that.development.length)) {
-// 			return false;
-// 		}
+		if (parseInt(this.development.length) != parseInt(that.development.length)) {
+			return false;
+		}
 		
-// 		for (var i in this.development) {
-// 			if (this.development[i] != that.development[i]) {
-// 				return false;
-// 			}
-// 		}
+		for (var i in this.development) {
+			if (this.development[i] != that.development[i]) {
+				return false;
+			}
+		}
 		
-// 		return true;
-// 	};
+		return true;
+	};
 	
-// 	// </PUBLIC>
-// }
+	// </PUBLIC>
+}
 
-// function BasicItem(rule, dotIndex) {
-// 	// <PUBLIC>
+function BasicItem(rule, dotIndex) {
+	// <PUBLIC>
 	
-// 	this.rule = rule;
+	this.rule = rule;
 	
-// 	this.dotIndex = dotIndex;
+	this.dotIndex = dotIndex;
 	
-// 	this.lookAheads = [];
+	this.lookAheads = [];
 	
-// 	this.addUniqueTo = function(items) {
-// 		return addUniqueUsingEquals(this, items);
-// 	};
+	this.addUniqueTo = function(items) {
+		return addUniqueUsingEquals(this, items);
+	};
 	
-// 	this.newItemsFromSymbolAfterDot = function() {
-// 		var result = [];
-// 		var nonterminalRules = this.rule.grammar.getRulesForNonterminal(this.rule.development[this.dotIndex]);
+	this.newItemsFromSymbolAfterDot = function() {
+		var result = [];
+		var nonterminalRules = this.rule.grammar.getRulesForNonterminal(this.rule.development[this.dotIndex]);
 		
-// 		for (var j in nonterminalRules) {
-// 			addUniqueUsingEquals(new Item(nonterminalRules[j], 0), result);
-// 		}
+		for (var j in nonterminalRules) {
+			addUniqueUsingEquals(new Item(nonterminalRules[j], 0), result);
+		}
 		
-// 		return result;
-// 	};
+		return result;
+	};
 	
-// 	this.newItemAfterShift = function() {
-// 		if (this.dotIndex < this.rule.development.length && this.rule.development[this.dotIndex] != EPSILON) {
-// 			return new Item(this.rule, this.dotIndex + 1);
-// 		}
+	this.newItemAfterShift = function() {
+		if (this.dotIndex < this.rule.development.length && this.rule.development[this.dotIndex] != EPSILON) {
+			return new Item(this.rule, this.dotIndex + 1);
+		}
 		
-// 		return undefined;
-// 	}
+		return undefined;
+	}
 	
-// 	this.equals = function(that) {
-// 		return this.rule.equals(that.rule) && (parseInt(this.dotIndex) == parseInt(that.dotIndex));
-// 	};
+	this.equals = function(that) {
+		return this.rule.equals(that.rule) && (parseInt(this.dotIndex) == parseInt(that.dotIndex));
+	};
 
-// 	this.toString = function() {
-// 		return this.rule.nonterminal + ' -> ' + this.rule.development.slice(0, this.dotIndex).join(' ') + '.' +
-// 				(isElement(EPSILON, this.rule.development) ? '' : this.rule.development.slice(this.dotIndex).join(' '));
-// 	};
+	this.toString = function() {
+		return this.rule.nonterminal + ' -> ' + this.rule.development.slice(0, this.dotIndex).join(' ') + '.' +
+				(isElement(EPSILON, this.rule.development) ? '' : this.rule.development.slice(this.dotIndex).join(' '));
+	};
 	
-// 	// </PUBLIC>
-// }
+	// </PUBLIC>
+}
 
-// function BasicLR1Item(rule, dotIndex) {
-// 	// <PUBLIC>
+function BasicLR1Item(rule, dotIndex) {
+	// <PUBLIC>
 	
-// 	extend(this, new BasicItem(rule, dotIndex));
+	extend(this, new BasicItem(rule, dotIndex));
 	
-// 	var zuper = this.zuper;
+	var zuper = this.zuper;
 	
-// 	this.lookAheads = rule.index == 0 ? ['$'] : [];
+	this.lookAheads = rule.index == 0 ? ['$'] : [];
 	
-// 	this.newItemsFromSymbolAfterDot = function() {
-// 		var result = this.zuper.newItemsFromSymbolAfterDot();
+	this.newItemsFromSymbolAfterDot = function() {
+		var result = this.zuper.newItemsFromSymbolAfterDot();
 		
-// 		if (result.length == 0) {
-// 			return result;
-// 		}
+		if (result.length == 0) {
+			return result;
+		}
 		
-// 		var newLookAheads = [];
-// 		var epsilonPresent = false;
-// 		var firstsAfterSymbolAfterDot = this.rule.grammar.getSequenceFirsts(this.rule.development.slice(this.dotIndex + 1));
+		var newLookAheads = [];
+		var epsilonPresent = false;
+		var firstsAfterSymbolAfterDot = this.rule.grammar.getSequenceFirsts(this.rule.development.slice(this.dotIndex + 1));
 		
-// 		for (var i in firstsAfterSymbolAfterDot) {
-// 			var first = firstsAfterSymbolAfterDot[i];
-// 			if (EPSILON == first) {
-// 				epsilonPresent = true;
-// 			} else {
-// 				addUnique(first, newLookAheads);
-// 			}
-// 		}
+		for (var i in firstsAfterSymbolAfterDot) {
+			var first = firstsAfterSymbolAfterDot[i];
+			if (EPSILON == first) {
+				epsilonPresent = true;
+			} else {
+				addUnique(first, newLookAheads);
+			}
+		}
 		
-// 		if (epsilonPresent) {
-// 			for (var i in this.lookAheads) {
-// 				addUnique(this.lookAheads[i], newLookAheads);
-// 			}
-// 		}
+		if (epsilonPresent) {
+			for (var i in this.lookAheads) {
+				addUnique(this.lookAheads[i], newLookAheads);
+			}
+		}
 		
-// 		for (var i in result) {
-// 			result[i].lookAheads = newLookAheads.slice(0);
-// 		}
+		for (var i in result) {
+			result[i].lookAheads = newLookAheads.slice(0);
+		}
 		
-// 		return result;
-// 	};
+		return result;
+	};
 	
-// 	this.newItemAfterShift = function() {
-// 		var result = zuper.newItemAfterShift();
+	this.newItemAfterShift = function() {
+		var result = zuper.newItemAfterShift();
 		
-// 		if (result != undefined) {
-// 			result.lookAheads = this.lookAheads.slice(0);
-// 		}
+		if (result != undefined) {
+			result.lookAheads = this.lookAheads.slice(0);
+		}
 		
-// 		return result;
-// 	}
+		return result;
+	}
 	
-// 	this.addUniqueTo = function(items) {
-// 		var result = false;
+	this.addUniqueTo = function(items) {
+		var result = false;
 		
-// 		for (var i in items) {
-// 			var item = items[i];
+		for (var i in items) {
+			var item = items[i];
 			
-// 			if (zuper.equals(item)) {
-// 				for (var i in this.lookAheads) {
-// 					result |= addUnique(this.lookAheads[i], item.lookAheads);
-// 				}
+			if (zuper.equals(item)) {
+				for (var i in this.lookAheads) {
+					result |= addUnique(this.lookAheads[i], item.lookAheads);
+				}
 				
-// 				return result;
-// 			}
-// 		}
+				return result;
+			}
+		}
 		
-// 		items.push(this);
+		items.push(this);
 		
-// 		return true;
-// 	};
+		return true;
+	};
 	
-// 	this.equals = function(that) {
-// 		return zuper.equals(that) && includeEachOther(this.lookAheads, that.lookAheads);
-// 	}
+	this.equals = function(that) {
+		return zuper.equals(that) && includeEachOther(this.lookAheads, that.lookAheads);
+	}
 
-// 	this.toString = function() {
-// 		return '[' + zuper.toString() + ', ' + this.lookAheads.join('/') + ']';
-// 	};
+	this.toString = function() {
+		return '[' + zuper.toString() + ', ' + this.lookAheads.join('/') + ']';
+	};
 	
-// 	// </PUBLIC>
-// }
+	// </PUBLIC>
+}
